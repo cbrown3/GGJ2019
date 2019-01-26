f49fefae005ab59321bfe1b82120e8380dd05f3f
cbb1320 <cbb1320@g.rit.edu>
2019-01-26 15:46:07 -0500

    Added Collision Interctions between Trees, Props, and Buildings
    Trees and props go flying when hit without the effect of gravity.
    Buildings make your car flip, strength of the flip depending on your speed.

--- Safe House/Assets/Scripts/CarCollision.cs
+++ Safe House/Assets/Scripts/CarCollision.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
 
 public class CarCollision : MonoBehaviour
 {
+    private CarController carController;
+    public float force;
     // Start is called before the first frame update
     void Start()
     {
-        
+        carController = GetComponent<CarController>();
     }
 
     // Update is called once per frame
@@ -18,19 +21,29 @@ public class CarCollision : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Building")
+        GameObject other = collision.gameObject;
+        if(other.tag == "Building")
         {
-            Debug.Log("You hit a building");
+            GetComponent<Rigidbody>().AddForce(Vector3.up * carController.CurrentSpeed, ForceMode.VelocityChange);
         }
-        if (collision.gameObject.tag == "NPC")
+        if (other.tag == "NPC")
         {
             Debug.Log("You hit an NPC");
         }
-        if (collision.gameObject.tag == "Tree")
+        if (other.tag == "Tree" || other.tag == "Prop")
         {
-            collision.gameObject.GetComponent<Rigidbody>();
+            //disable the collider
+            other.GetComponent<Collider>().enabled = false;
+            //get angle between collision point and the player
+            Vector3 dir = collision.contacts[0].point - transform.position;
+            //get the opposite direction and normalize
+            dir = -dir.normalized;
+            //deactivate gravity
+            other.GetComponent<Rigidbody>().useGravity = false;
+            //add the force to the tree
+            other.GetComponent<Rigidbody>().AddForce(dir * carController.CurrentSpeed * -10.0f);
         }
-        if (collision.gameObject.tag == "Car")
+        if (other.tag == "Car")
         {
             Debug.Log("You hit a car");
         }
