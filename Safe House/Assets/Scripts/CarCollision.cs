de422d644b3dfc0c4ee4960abf249d8eeff91a02
cbb1320 <cbb1320@g.rit.edu>
2019-01-26 12:03:16 -0500

    Collision Tags

--- Safe House/Assets/Scripts/CarCollision.cs
+++ Safe House/Assets/Scripts/CarCollision.cs
@@ -15,4 +15,24 @@ public class CarCollision : MonoBehaviour
     {
         
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Building")
+        {
+            Debug.Log("You hit a building");
+        }
+        if (collision.gameObject.tag == "NPC")
+        {
+            Debug.Log("You hit an NPC");
+        }
+        if (collision.gameObject.tag == "Tree")
+        {
+            collision.gameObject.GetComponent<Rigidbody>();
+        }
+        if (collision.gameObject.tag == "Car")
+        {
+            Debug.Log("You hit a car");
+        }
+    }
 }
