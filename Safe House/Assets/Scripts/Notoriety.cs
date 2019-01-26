880c56475b3c53357402359ea6e3bcc585296fba
Nicholas Bazos (RIT Student) <ndb9897@ad.rit.edu>
2019-01-26 15:43:12 -0500

    Game states, police pursuit, menus

--- Safe House/Assets/Scripts/Notoriety.cs
+++ Safe House/Assets/Scripts/Notoriety.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Notoriety : MonoBehaviour
 {
+    public GameObject policeChopper;
+    public float movementSpeed = 25;
+    private bool pursuit;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pursuit = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(pursuit)
+        {
+            Debug.Log("Cops are in pursuit!");
+
+            policeChopper.transform.LookAt(gameObject.transform);
+            Vector3 chopperPos = policeChopper.transform.position;
+            chopperPos += -transform.forward * movementSpeed * Time.deltaTime;
+            chopperPos = new Vector3(chopperPos.x, 33, chopperPos.z);
+            policeChopper.transform.position = chopperPos;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!other.name.Contains("Street"))
+        {
+            pursuit = true;
+        }
     }
 }
--- Safe House/Assets/Scripts/Timer.cs
+++ Safe House/Assets/Scripts/Timer.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
     public Text timer;
-    public float minutes = 5;
-    public float seconds = 0;
-    public float milliseconds = 0;
+    public float minutes;
+    public float seconds;
+    public float milliseconds;
+    public bool start = false;
 
     /** the time on the timer that will switch the color from green to yellow in minutes **/
     public int GreentoYellow;
@@ -28,56 +30,64 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (milliseconds <= 0)
+        if (start)
         {
-            if (seconds <= 0)
+            if (milliseconds <= 0 && seconds <= 0 && minutes <= 0)
             {
-                minutes--;
-                seconds = 59;
-            }
-            else if (seconds >= 0)
-            {
-                seconds--;
+                SceneManager.LoadScene(2);
             }
 
-            milliseconds = 100;
-        }
+            if (milliseconds <= 0)
+            {
+                if (seconds <= 0)
+                {
+                    minutes--;
+                    seconds = 59;
+                }
+                else if (seconds >= 0)
+                {
+                    seconds--;
+                }
 
-        milliseconds -= Time.deltaTime * 100;
+                milliseconds = 100;
+            }
 
-        string minuteStr = minutes.ToString();
-        string secondStr = seconds.ToString();
-        string millisecondStr = milliseconds.ToString();
+            milliseconds -= Time.deltaTime * 100;
 
-        if(seconds < 10)
-        {
-            secondStr = "0" + secondStr;
-        }
+            string minuteStr = minutes.ToString();
+            string secondStr = seconds.ToString();
+            string millisecondStr = milliseconds.ToString();
 
-        if(milliseconds < 10)
-        {
-            millisecondStr = "0" + millisecondStr;
-        }
+            if (seconds < 10)
+            {
+                secondStr = "0" + secondStr;
+            }
 
-        millisecondStr = millisecondStr.Substring(0, 2);
+            if (milliseconds < 10)
+            {
+                millisecondStr = "0" + millisecondStr;
+            }
 
-        timer.text = minuteStr + ":" + secondStr + ":" + millisecondStr;
+            millisecondStr = millisecondStr.Substring(0, 2);
 
-        //Update the color
+            timer.text = minuteStr + ":" + secondStr + ":" + millisecondStr;
 
-        if (minutes <= 0 && seconds < YellowtoRed)
-        {
-            timer.CrossFadeColor(Color.red, 0.1f, true, false);
-            return;
-        }
-        else if(minutes < GreentoYellow)
-        {
-            timer.CrossFadeColor(Color.yellow, 0.1f, true, false);
-            return;
-        }
-        else
-        {
-            timer.color = Color.green;
+            //Update the color
+
+            if (minutes <= 0 && seconds < YellowtoRed)
+            {
+                timer.CrossFadeColor(Color.red, 0.1f, true, false);
+                return;
+            }
+            else if (minutes < GreentoYellow)
+            {
+                timer.CrossFadeColor(Color.yellow, 0.1f, true, false);
+                return;
+            }
+            else
+            {
+                timer.color = Color.green;
+            }
         }
     }
 
@@ -99,6 +109,11 @@ public class Timer : MonoBehaviour
             i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
             yield return null;
         }
+    }
+
+    public void setStart(bool start)
+    {
+        this.start = start;
     }
 }
 
