108f7703a4607b1e21d7ff4c43e1d096344fb923
Nicholas Bazos <nbazos15@gmail.com>
2019-01-26 00:25:52 -0500

    timer

---
+++ Safe House/Assets/Scripts/Timer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public Text timer;
+    public float minutes = 5;
+    public float seconds = 0;
+    public float miliseconds = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (miliseconds <= 0)
+        {
+            if (seconds <= 0)
+            {
+                minutes--;
+                seconds = 59;
+            }
+            else if (seconds >= 0)
+            {
+                seconds--;
+            }
+
+            miliseconds = 100;
+        }
+
+        miliseconds -= Time.deltaTime * 100;
+
+        timer.text = string.Format("{0}:{1}:{2}", minutes, seconds, (int)miliseconds);
+    }
+}
